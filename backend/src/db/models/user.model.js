bd7002c3cf3915bf217c51cccf65bf19e9a7b043
Bibek Ray <bibek.ray@deerwalk.edu.np>
2026-02-14 18:40:52 +0545

    feat(db): initialize database connection and user model
    - Added a new database connection file to set up drizzle ORM with PostgreSQL.
    - Created a user model defining the structure of the users table with fields for id, firstname, lastname, email, password, salt, createdAt, and updatedAt.
    - Exported the users table from the models index for easier access.

---
+++ backend/src/db/models/user.model.js
@@ -0,0 +1,16 @@
+import { pgTable, varchar, uuid, text, timestamp } from "drizzle-orm/pg-core";
+
+export const usersTable = pgTable("users", {
+  id: uuid().primaryKey().defaultRandom(),
+
+  firstname: varchar("first_name", { length: 50 }).notNull(),
+  lastname: varchar("last_name", { length: 50 }),
+  
+  email: varchar({ length: 255 }).notNull().unique(),
+  
+  password: text().notNull(),
+  salt: text().notNull(),
+
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").$onUpdate(() => new Date()),
+});
